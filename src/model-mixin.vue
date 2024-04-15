b509262a1b7adf01e13bf9a513df07b7fb326bf8
oreo <Tang.Li5@geely.com>
2024-04-15 11:46:09 +0800

    fix: mobile bug

--- src/model-mixin.vue
+++ src/model-mixin.vue
@@ -129,7 +129,7 @@
         @mouseover="showInfo"
         onmouseout="this.style.color = '#565266'"
         @mouseout="hideInfo"
-        v-on:touchstart="toggleWireframeMode"
+        v-on:touchstart="isShowInfo ? hideInfo : showInfo"
       >
         <svg viewBox="64 64 896 896" focusable="false" data-icon="info-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path><path d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z"></path></svg>
       </div>
@@ -709,7 +709,6 @@ export default defineComponent({
         if (!object) return;
 
         const distance = getSize(object).length();
-        console.log('getSize', getSize(object));
 
         camera.position.set(this.cameraPosition.x, this.cameraPosition.y, this.cameraPosition.z);
         camera.rotation.set(this.cameraRotation.x, this.cameraRotation.y, this.cameraRotation.z);
@@ -929,10 +928,11 @@ export default defineComponent({
       this.$refs['interaction-prompt'].style.opacity = 0;
     },
     showInfo() {
+      this.isShowInfo = true
       this.$refs['info-window'].style.opacity = 1
-      
     },
     hideInfo() {
+      this.isShowInfo = false
       this.$refs['info-window'].style.opacity = 0
     },
     updateObjectInfo() {
